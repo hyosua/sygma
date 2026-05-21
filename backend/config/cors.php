bb6091f12d150880e68d35b0f3996ae68caab5e4
hyo <colleterhyosua@gmail.com>
2026-05-21 19:20:05 +0200

    chore: ajout du lien vercel dans la config cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173', 'https://sygma-eight.vercel.app')],
 
     'max_age' => 0,
 
